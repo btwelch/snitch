936bcbed5c14242511e0985b2e75a8fd5bbe55fb
Barry Welch <barry.welch@logmein.com>
2016-11-04 14:42:08 -0500

    Groups and ordering

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,16 @@ var express = require('express');
 var router  = express.Router();
 
 router.get('/', function(req, res) {
+
   models.Server.findAll({where: { group: 1 }}).then(function(group1_servers) {
     models.Server.findAll({where: { group: 2 }}).then(function(group2_servers) {
-     res.render('index', {
-       title: 'Snitch',
-       group1_servers: group1_servers,
-       group2_servers: group2_servers       
-     });
-   });
+      res.render('index', {
+        title: 'Snitch',
+        group1_servers: group1_servers,
+        group2_servers: group2_servers       
+      });
+    });
+  });
 });
 
 module.exports = router;
